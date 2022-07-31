66e7cc5fb16c67abb5697e1aea6f1a4d0f2eb75f
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2022-07-31 16:43:03 +0900

    [Bronze V] Title: A+B - 4, Time: 144 ms, Memory: 14664 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+     InputStreamReader input = new InputStreamReader(System.in);
+     BufferedReader br = new BufferedReader(input);
+     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        String line = "";
+
+        while((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a + b + "\n");
+        }
+
+        bw.flush();
+
+        input.close();
+        br.close();
+        bw.close();
+    }
+}
