f8aea1769585ed6708332fa8d009d32fcb53ee1b
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2022-08-16 01:10:08 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 28 ms, Memory: 423060 KB -BaekjoonHub

---
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Test {
+     long sum(int[] a) {
+        long b = 0;
+        for(int i = 0; i < a.length; i++)
+            b+=a[i];
+        return b;
+    }
+}
