1f5b43ce31842577380e4398fe6658003ede4d5e
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2022-08-21 03:28:34 +0900

    [Bronze V] Title: 오늘 날짜, Time: 112 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.time.LocalDate;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("2022-08-21");
+
+    }
+}
