94d1fb1a544c3c40d79f72bd22fbd30c42b1560d
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2022-08-21 03:31:40 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.time.LocalDate;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        OutputStreamWriter out = new OutputStreamWriter(System.out);
+        BufferedWriter bw = new BufferedWriter(out);
+
+
+        System.out.println("고려대학교");
+
+
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
