05827b46922e745b49a1bfee7830e91cffcafeef
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2022-09-03 02:21:06 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 184 ms, Memory: 16640 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+    public static void shit(BufferedWriter bw, int N, int n) throws IOException {
+        String typen = "";
+        for(int i = 0; i < n; i++) typen += "____";
+
+        bw.write(typen + "\"재귀함수가 뭔가요?\"" + "\n");
+        if(n == N) {
+            bw.write(typen + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+        }
+        else {
+            bw.write(typen + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" +
+                     typen +"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" +
+                    typen +"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" + "\n");
+
+            shit(bw,N,n+1);
+        }
+        bw.write(typen + "라고 답변하였지.\n");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        OutputStreamWriter out = new OutputStreamWriter(System.out);
+        BufferedWriter bw = new BufferedWriter(out);
+
+        int N = Integer.parseInt(br.readLine());
+
+        bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." + "\n");
+        shit(bw, N, 0);
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
