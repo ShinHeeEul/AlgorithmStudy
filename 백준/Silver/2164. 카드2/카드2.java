9c97b9e8e7a5e99d09717c958ad9bf5d1815042f
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-03-09 16:40:14 +0900

    [Silver IV] Title: 카드2, Time: 268 ms, Memory: 48996 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 1; i < N+1; i++) queue.add(i);
+
+        while(queue.size() != 1) {
+            queue.remove();
+            queue.add(queue.remove());
+        }
+
+        System.out.println(queue.remove());
+
+    }
+}
