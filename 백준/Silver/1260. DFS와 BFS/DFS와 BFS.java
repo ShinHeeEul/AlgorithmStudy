531855990d88d3b2e985f7020df3611bc3b44153
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-04-06 20:47:44 +0900

    [Silver II] Title: DFS와 BFS, Time: 496 ms, Memory: 25572 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,63 +1,69 @@
+import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static int node[][]; // 그래프 배열
-    static int check[]; // 방문 배열
-    static void bfs(int start) { // BFS 메소드
-        Queue<Integer> queue = new LinkedList<>();
 
-        System.out.print(start + " ");
-        check[start] =1;
-        queue.offer(start);
-        while(!queue.isEmpty()) {
-            int x = queue.poll();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-            for(int i=1;i<node.length;i++) { // 차례대로 1번과 연결 된 컴퓨터들을 찾아 cnt변수 증가
-                if(node[x][i]==1 && check[i]!=1) {
-                    System.out.print(i + " ");
-                    check[i] = 1;
-                    queue.add(i);
-                }
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int[][] node = new int[N+1][N+1];
+        int[] check = new int[N+1];
+
+        for(int i = 0 ; i< M; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            node[a][b] = 1;
+            node[b][a] = 1;
+        }
+
+        //시작 위치 : a, 도착 위치 : b
+        //dfs
+
+        int current = V;
+        Stack<Integer> stack = new Stack<>();
+        stack.add(current);
+        while(!stack.isEmpty()) {
+            int tmp = stack.pop();
+            if(check[tmp] == 0) {
+                System.out.print(tmp + " ");
+                check[tmp] = 1;
+            }
+            current = tmp;
+            for(int j = N; j > 0; j--) {
+                if((check[j] == 0) && (node[current][j] == 1)) stack.add(j);
             }
         }
-    }
 
-    static void dfs(int start) {
+        System.out.println();
 
-        System.out.print(start + " ");
-        check[start] = 1;
-
-             for(int i = 1; i < node.length;i++) {
-                 if(node[start][i] == 1 && check[i] != 1) {
-                     dfs(i);
-                 }
-             }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n= sc.nextInt(); // 컴퓨터의 수
-        int m = sc.nextInt(); // 네트워크 상에 연결되어 있는 컴퓨터 쌍의 수 즉, 간선의 수
-        int start = sc.nextInt();
-
-        node=new int[n+1][n+1];
-        check = new int[n+1];
-        for(int i=0;i<m;i++) { // 그래프 구성
-            int a=sc.nextInt();
-            int b = sc.nextInt();
-            node[a][b]=1;
-            node[b][a]=1;
+       //bfs
+       current = V;
+       check = new int[N+1];
+       Queue<Integer> queue = new LinkedList<>();
+       queue.add(current);
+        while(!queue.isEmpty()) {
+            int tmp = queue.poll();
+            if(check[tmp] == 0) {
+                System.out.print(tmp + " ");
+                check[tmp] = 1;
+            }
+            current = tmp;
+            for(int j = 1; j < N+1; j++) {
+                if((check[j] == 0) && (node[current][j] == 1)) queue.add(j);
+            }
 
         }
 
-        dfs(start);
-        System.out.println();
-        check = new int[n+1];
-        bfs(start);
 
-
+        bw.flush();
+        bw.close();
     }
 
 }
