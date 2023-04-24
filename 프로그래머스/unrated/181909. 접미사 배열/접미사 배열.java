7e66cc9c02b2cb950eb70e766397ec84cb8f349d
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-04-24 21:54:16 +0900

    [unrated] Title: 접미사 배열, Time: 0.21 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> solution(String my_string) {
+        ArrayList<String> answer = new ArrayList();
+        for(int i = 0; i < my_string.length(); i++) {
+            answer.add(my_string.substring(i, my_string.length()));
+        }
+        
+        Collections.sort(answer);
+        return answer;
+    }
+}
