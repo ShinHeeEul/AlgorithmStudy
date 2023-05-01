dcf13826ae0e8bc9a44a2bcdae633f676c81a20b
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-05-01 23:40:56 +0900

    [unrated] Title: 이어 붙인 수, Time: 12.42 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        for(int i : num_list) {
+            if(i % 2 != 0) odd += i;
+            else even += i;
+        }
+        answer = Integer.parseInt(odd) + Integer.parseInt(even);
+        return answer;
+    }
+}
