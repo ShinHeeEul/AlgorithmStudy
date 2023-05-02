a362c047b12ffc80f4fe006ef7042846161b9ab5
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-05-02 14:39:48 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.56 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        for(char s : control.toCharArray()) {
+            switch(s) {
+                case 'w':
+                    n++;
+                    break;
+                case 's':
+                    n--;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+            }
+            
+        }
+            return n;
+    }
+}
