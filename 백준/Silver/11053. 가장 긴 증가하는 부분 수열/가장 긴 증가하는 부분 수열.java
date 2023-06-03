e37467cdb991ed294c159cfe9a64b4f2cb1f03af
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-06-03 16:48:12 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,13 +7,13 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        
-        //arr 맨 처음 값이 담길 array
-        int[] arr = new int[N + 1];
-        //dp 횟수 값이 담길 array
-        int[] dp = new int[N + 1];
+
+        // arr 맨 처음 값이 담길 array
+        int[] arr = new int[N];
+        // dp 횟수 값이 담길 array
+        int[] dp = new int[N];
         st = new StringTokenizer(br.readLine());
-        //모든 dp 값을 1로 초기화
+        // 모든 dp 값을 1로 초기화
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             dp[i] = 1;
@@ -21,20 +21,18 @@ class Main {
 
         // i는 현재 index, j는 i전까지의 index
         // 만약 arr[i]보다 arr[j]가 작다면(arr[j] 다음 부분 원소가 arr[i]가 될 수 있음)
-        //dp[j] + 1과 dp[i] 중 큰 값으로 dp[i] 초기화
+        // dp[j] + 1과 dp[i] 중 큰 값으로 dp[i] 초기화
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                if(arr[i] > arr[j]){
+                if(arr[i] > arr[j])
                     dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
             }
         }
         int ans = -1;
-        for(int i = 0 ; i < N; i++){
+        for(int i = 0 ; i < N; i++)
             ans = Math.max(ans, dp[i]);
-        }
 
         System.out.println(ans);
     }
-    
+
 }
