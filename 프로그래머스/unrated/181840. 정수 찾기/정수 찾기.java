25ee146ee9eef7b9e173ec4539389ece034d616e
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-09-07 23:49:21 +0900

    [unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        ArrayList<Integer> numList = new ArrayList();
+        
+        for(int i = 0 ; i < num_list.length ; i++) {
+            if(num_list[i] == n) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
