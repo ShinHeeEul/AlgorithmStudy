d70b1e4900b532dfed5f600eab917ab9f04643e9
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-09-19 11:37:59 +0900

    [unrated] Title: 두 수의 합, Time: 246.73 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.*;
+
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger c = new BigInteger(a);
+        BigInteger d = new BigInteger(b);
+        
+        return c.add(d).toString();
+    }
+}
