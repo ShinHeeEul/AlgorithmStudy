f818d9fa208c5b90093c9537dc904cf6767e23c0
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-09-24 14:51:55 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+import java.math.*;
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        for(int i = 0; i < t; i++) {
+            answer *= 2;
+        }
+        return answer;
+    }
+}
