91d9f8558ed9c78fa98a5dc91734ada8adaf2e64
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-15 19:26:58 +0900

    [level unrated] Title: 숫자 찾기, Time: 5.52 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num, int k) {
+        String s = num + "";
+        for(int i = 0; i < s.length(); i++) {
+            int tt =  Integer.parseInt(s.charAt(i) + "");
+            
+            if(tt == k) {
+                return i+1;
+            }
+        }
+        
+        return -1;
+    }
+}
