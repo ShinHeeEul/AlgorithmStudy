30ffd3788e4cc81e86940f6547f88f0df6c3e9ec
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-18 17:52:08 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        for(int i = 1; i < 100; i++) {
+            if((i * 6) % n == 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
