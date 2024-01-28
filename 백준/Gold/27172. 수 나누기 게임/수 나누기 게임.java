7b7316f82f4e6505dd3271e8ef432f3bc54e09de
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-01-28 18:39:04 +0900

    [Gold V] Title: 수 나누기 게임, Time: 696 ms, Memory: 33388 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        int INF = 1000001;
+        int[] point = new int[INF];
+        for(int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            for(int j = 2; j * a < INF; j++) point[j * a]--;
+
+            for(int j = 1; j * j <= a; j++) {
+                if(a % j == 0) {
+                    point[j]++;
+                    if(j != a / j) {
+                        point[a / j]++;
+                    }
+                }
+            }
+            point[a]--;
+            arr[i] = a;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            sb.append(point[arr[i]]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
