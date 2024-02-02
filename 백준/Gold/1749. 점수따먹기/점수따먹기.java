8162026196422547cb9b71135841ca0ada971f34
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-02-02 18:11:25 +0900

    [Gold IV] Title: 점수따먹기, Time: 1624 ms, Memory: 21608 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[][] dp = new long[N+1][M+1];
+        long answer = -Long.MIN_VALUE;
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                if(answer < tmp) {
+                    answer = tmp;
+                }
+                dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] + tmp- dp[i][j];
+                if(answer < dp[i+1][j+1]) {
+                    answer = dp[i+1][j+1];
+                }
+            }
+        }
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= M; j++) {
+                for(int k = N; k >= i; k--) {
+                    for(int l = M; l >= j; l--) {
+                        long tmp;
+                        if(i == 1) {
+                            tmp = dp[k][l] - dp[k][j-1] - dp[i-1][l] + dp[i][j-1];
+                        } else {
+                            tmp = dp[k][l] - dp[k][j - 1] - dp[i - 1][l] + dp[i - 1][j - 1];
+                        }
+                        if(tmp > answer){
+                            answer = tmp;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
+
+}
