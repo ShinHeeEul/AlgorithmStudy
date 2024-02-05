569caa0641bfd3d5be140fd30e370dc976a91088
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-02-05 17:22:29 +0900

    [Gold V] Title: 압축, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/1662. 압축/압축.java
+++ 백준/Gold/1662. 압축/압축.java
@@ -7,22 +7,21 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        
+
         int tmp = 0;
         Stack<Integer> stack = new Stack<>();
-        Stack<Integer> multipleStack = new Stack<>();
         char before = 0;
 
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
+
             if(c == '(') {
-                stack.push(tmp);
-                multipleStack.push(Integer.parseInt(before + ""));
+                stack.push(tmp * 10 + Integer.parseInt(before + ""));
                 tmp = 0;
             } else if(c == ')') {
-                tmp *= multipleStack.pop();
-                tmp += stack.pop()-1;
+                int pop = stack.pop();
+                tmp *= pop % 10;
+                tmp += pop/10-1;
             } else {
                 tmp++;
                 before = c;
