5161f4754af257fdc66282b05cfa1422548a2832
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-02-08 16:58:53 +0900

    [Gold V] Title: 흙길 보수하기, Time: 160 ms, Memory: 14892 KB -BaekjoonHub

---
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int L = read();
+
+        ArrayList<Node> list = new ArrayList<>();
+
+        for(int i = 0; i < N; i++) {
+            list.add(new Node(read(), read()));
+        }
+
+        Collections.sort(list);
+        int answer = 0;
+        int before = -1;
+        for(Node n : list) {
+            if(n.start <= before) n.start = before + 1;
+            int a = n.end - n.start;
+
+            if(a <= 0) continue;
+            if(a % L == 0) {
+                answer += a / L;
+            } else {
+                answer += a / L + 1;
+                before = n.end + L - a % L - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static class Node implements Comparable<Node>{
+        int start;
+        int end;
+
+        public Node(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.start - o.start;
+        }
+    }
+
+    private static int read() throws Exception {
+        int d, o = System.in.read() & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+        return o;
+    }
+}
