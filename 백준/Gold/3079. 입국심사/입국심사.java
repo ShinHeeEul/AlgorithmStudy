84f303cf5356c0ae5d12384d6c299b5427951ca1
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-02-09 17:27:54 +0900

    [Gold V] Title: 입국심사, Time: 220 ms, Memory: 14864 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,38 +1,38 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = read();
+        int M = read();
 
         long start = 0;
         long end = Integer.MAX_VALUE;
         int[] arr = new int[N];
         for(int i = 0 ; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = read();
             if(end > arr[i]) end = arr[i];
         }
         end *= M;
 
         while(start < end) {
-            long mid = (end + start) / 2;
+            long mid = (start + end) / 2;
             long ans = 0;
-
             for(int i : arr) ans += mid/i;
 
-            if(ans >= M) {
-                end = mid;
-            } else {
+            if(ans < M) {
                 start = mid + 1;
+            } else {
+                end = mid;
             }
         }
 
         System.out.println(start);
 
+    }
+
+    private static int read() throws Exception {
+        int d, o = System.in.read() & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+        return o;
     }
 }
