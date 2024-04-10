30915f0e36ab5fd9dfca5da1d053a21cad63d9f9
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-04-10 17:57:22 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 240 ms, Memory: 16348 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,18 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.BitSet;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int N = read();
         int[] arr = new int[N+1];
         long cnt = 0;
         BitSet check = new BitSet(100001);
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        
+        for (int i = 1; i <= N; i++) arr[i] = read();
 
         for(int start=1, end=0; start <= N; start++) {
             while(end+1<=N && !check.get(arr[end+1])) {
@@ -24,5 +20,12 @@ public class Main {
         }
 
         System.out.println(cnt);
+    }
+
+    private static int read() throws Exception {
+        int d, o = System.in.read() & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+        return o;
     }
 }
