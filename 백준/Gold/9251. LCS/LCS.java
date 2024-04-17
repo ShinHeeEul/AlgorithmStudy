96aaeb9a8249a0ec5b088048ebd1c3c457fb3c0f
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-04-17 17:41:34 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 18432 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,32 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    static int[] weight;
-    static boolean[] visited;
-    static int N;
-    static int M;
-    static int K;
-    static int min = Integer.MAX_VALUE;
-    static ArrayList<Integer> list;
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String s1 = br.readLine();
         String s2 = br.readLine();
-        int[][] arr = new int[s1.length()+1][s2.length()+1];
+        int N = s1.length();
+        int M = s2.length();
+        int[][] arr = new int[N+1][M+1];
 
-        for(int i = 1; i < arr.length; i++) {
-            for(int j = 1; j < arr[0].length; j++) {
-                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+        for(int i = 1; i <= N; i++) {
+            char c1 = s1.charAt(i-1);
+
+            for(int j = 1; j <= M; j++) {
+                char c2 = s2.charAt(j-1);
+
+                if(c1 == c2) {
                     arr[i][j] = arr[i-1][j-1] + 1;
-                } else {
-                    arr[i][j] = Math.max(arr[i][j - 1], arr[i - 1][j]);
+                    continue;
                 }
+                arr[i][j] = Math.max(arr[i][j-1], arr[i-1][j]);
+
             }
         }
-        System.out.println(arr[s1.length()][s2.length()]);
+        System.out.println(arr[N][M]);
     }
 }
