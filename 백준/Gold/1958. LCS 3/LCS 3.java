80a4bc800edb8d5bb92f1ea120c970bf0961f96d
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-04-17 19:26:35 +0900

    [Gold IV] Title: LCS 3, Time: 180 ms, Memory: 20196 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.java
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -5,30 +5,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String s1 = br.readLine();
         String s2 = br.readLine();
         String s3 = br.readLine();
+        int N = s1.length();
+        int M = s2.length();
+        int K = s3.length();
 
-        int[][][] dp = new int[s1.length()+1][s2.length()+1][s3.length()+1];
+        int[][][] dp = new int[N+1][M+1][K+1];
 
-        for(int i = 1; i <= s1.length(); i++) {
+        for(int i = 1; i <= N; i++) {
 
-            for(int j = 1; j <= s2.length(); j++) {
+            for(int j = 1; j <= M; j++) {
 
-                for(int k = 1; k <= s3.length(); k++) {
+                for(int k = 1; k <= K; k++) {
 
                     if((s1.charAt(i-1) == s2.charAt(j-1))
                             && (s1.charAt(i-1) == s3.charAt(k-1))) {
                         dp[i][j][k] = dp[i-1][j-1][k-1] + 1;
-                    } else {
-                        dp[i][j][k] = Math.max(dp[i-1][j][k], Math.max(dp[i][j-1][k], dp[i][j][k-1]));
+                        continue;
                     }
+                    dp[i][j][k] = Math.max(dp[i-1][j][k], Math.max(dp[i][j-1][k], dp[i][j][k-1]));
+
                 }
             }
         }
 
-        System.out.println(dp[s1.length()][s2.length()][s3.length()]);
+        System.out.println(dp[N][M][K]);
     }
 }
