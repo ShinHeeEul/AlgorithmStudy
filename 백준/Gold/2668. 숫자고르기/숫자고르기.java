2f8a55e1e9c70e18ae8d6ad3f514e97ef51618bc
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-04-23 20:54:31 +0900

    [Gold V] Title: 숫자고르기, Time: 128 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class Main {
+    static boolean[] visited;
+    static int[] arr;
+    static int current;
+    static Set<Integer> set;
+    static boolean b;
+    public static void main(String[] args) throws Exception {
+
+        int N = read();
+
+        arr = new int[N+1];
+        visited = new boolean[N+1];
+        set = new LinkedHashSet<>();
+        for(int i = 1; i <= N; i++) {
+            arr[i] = read();
+        }
+
+        for(int i = 1; i <= N; i++) {
+            b = false;
+            if(!visited[i]) {
+                current = i;
+                dfs(i);
+            }
+        }
+        System.out.println(set.size());
+        set.stream().sorted().forEach(System.out::println);
+    }
+
+    private static void dfs(int start) {
+        if(visited[start]) {
+            return;
+        }
+
+        visited[start] = true;
+        int a = arr[(start)];
+        if(visited[a] && a == current) {
+            set.add(start);
+            b = true;
+            return;
+        }
+        dfs(a);
+        if(b) {
+            set.add(start);
+            return;
+        }
+        visited[start] = false;
+
+    }
+
+    private static int read() throws Exception {
+        int d, o = System.in.read() & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+        return o;
+    }
+}
