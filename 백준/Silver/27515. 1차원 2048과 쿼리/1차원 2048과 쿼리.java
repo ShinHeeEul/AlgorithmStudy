dd28b50593931fab93d33f69c75c11365ae97ed3
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-05-03 23:22:52 +0900

    [Silver II] Title: 1차원 2048과 쿼리, Time: 812 ms, Memory: 74508 KB -BaekjoonHub

--- 백준/Silver/27515. 1차원 2048과 쿼리/1차원 2048과 쿼리.java
+++ 백준/Silver/27515. 1차원 2048과 쿼리/1차원 2048과 쿼리.java
@@ -14,26 +14,19 @@ public class Main {
             long l = read();
             if(l != 0) {
                 int index = (int) (Math.log((double) l) / Math.log(2));
-                if(index >= 0 && index <= N-1) {
-                    arr[index]= arr[index] + (c=='+'?1 : -1);
-                }
+                arr[index] += (c=='+'?1 : -1);
             }
-            int[] tmp = arr.clone();
+            int max = 0;
+            int next = 0;
             for(int j = 0; j < N-1; j++) {
-                if(tmp[j] >= 2) {
-                    tmp[j + 1] += tmp[j] / 2;
-                }
+                next = (arr[j] + next) / 2;
+                if(arr[j + 1] + next > 0) max = Math.max(max, j + 1);
             }
-            for(int j = N-1; j >= 0; j--) {
-                if(j == 0 && tmp[j] == 0) {
-                    sb.append(0).append("\n");
-                    break;
-                }
-                if(tmp[j] >= 1) {
-                    sb.append(Double.valueOf(Math.pow(2, j)).longValue()).append("\n");
-                    break;
-                }
+            if(arr[0] == 0 && max == 0) {
+                sb.append(0).append("\n");
+                continue;
             }
+            sb.append(Double.valueOf(Math.pow(2, max)).longValue()).append("\n");
         }
         System.out.println(sb);
     }
