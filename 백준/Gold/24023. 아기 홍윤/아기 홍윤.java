62b066a4949708f924d6b77d712d417961f7a4ac
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-05-07 18:48:25 +0900

    [Gold V] Title: 아기 홍윤, Time: 204 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Gold/24023. 아기 홍윤/아기 홍윤.java
+++ 백준/Gold/24023. 아기 홍윤/아기 홍윤.java
@@ -1,15 +1,12 @@
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        int N = read();
+        long N = read();
         long K = read();
-        long ans = 0;
+        long ans = 0L;
         int start = 1;
         for(int i = 1; i <= N; i++) {
-            long a = read();
-            ans |= a;
-            //자릿수가 같지 않으면? -> 크든, 같지 않든 K랑 연산하면 뭔가 다른 값이 나오겠지?
-            //그러면 그 수 제외하고 계산
+            ans |= read();
             if((ans | K) != K) {
                 start = i + 1;
                 ans = 0L;
@@ -23,8 +20,8 @@ public class Main {
         System.out.println(-1);
         }
 
-    private static int read() throws Exception {
-        int d, o = System.in.read() & 15;
+    private static long read() throws Exception {
+        long d, o = System.in.read() & 15;
         while ((d = System.in.read()) > 32)
             o = (o << 3) + (o << 1) + (d & 15);
         return o;
