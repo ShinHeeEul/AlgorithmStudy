fada0a3faaa5fb78901331667efc7c75bc59f890
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-05-11 16:56:34 +0900

    [Gold II] Title: 물통, Time: 1228 ms, Memory: 351952 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14867. 물통/물통.java
+++ 백준/Gold/14867. 물통/물통.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,14 +8,37 @@ public class Main {
     static int B;
     static int C;
     static int D;
-    static HashMap<Node, Boolean> map = new HashMap<>();
+    static HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
     public static void main(String[] args) throws Exception {
         A = read();
         B = read();
         C = read();
         D = read();
 
-        bfs();
+        if ((A == C && B == D) || (A == D && B == C)) {
+            System.out.println(2);
+            return;
+        }
+        int gcd = gcd(Math.min(A, B), Math.max(A, B));
+        if ((C % gcd == 0) || (C == 0) || (D % gcd == 0) || (D == 0)) {
+            bfs();
+            return;
+        }
+
+
+        System.out.println(-1);
+    }
+
+    public static int gcd(int a, int b) {
+        int gcd = 1;
+        for(int i = a; i >= 1; i--) {
+            if ((b % i == 0) && (a % i == 0)) {
+                gcd *= i;
+                b /= i;
+                a /= i;
+            }
+        }
+        return gcd;
     }
 
     public static class Node {
@@ -44,11 +68,12 @@ public class Main {
 
         Node start = new Node(0,0, 0);
         queue.add(start);
-        map.put(start, true);
+        HashSet<Integer> tmp = map.getOrDefault(start.A, new HashSet<>());
+        tmp.add(0);
+        map.put(start.A, tmp);
 
         while(!queue.isEmpty()) {
             Node n = queue.poll();
-
             if((n.A == C) && (n.B == D)) {
                 System.out.println(n.count);
                 return;
@@ -56,36 +81,48 @@ public class Main {
 
             // F(X) : 물통 X를 가득 채움
             Node node = new Node(A , n.B, n.count + 1);
-            if(!map.getOrDefault(node, false)) {
-                map.put(node, true);
+            tmp = map.getOrDefault(node.A, new HashSet<>());
+            if(!tmp.contains(node.B)) {
+                tmp.add(node.B);
                 queue.add(node);
+                map.put(node.A, tmp);
             }
             node = new Node(n.A, B, n.count + 1);
-            if(!map.getOrDefault(node, false)) {
-                map.put(node, true);
+            tmp = map.getOrDefault(node.A, new HashSet<>());
+            if(!tmp.contains(node.B)) {
+                tmp.add(node.B);
                 queue.add(node);
+                map.put(node.A, tmp);
             }
             // E(X) : 물통 X를 비움
             node = new Node(0, n.B, n.count + 1);
-            if(!map.getOrDefault(node, false)) {
-                map.put(node, true);
+            tmp = map.getOrDefault(node.A, new HashSet<>());
+            if(!tmp.contains(node.B)) {
+                tmp.add(node.B);
                 queue.add(node);
+                map.put(node.A, tmp);
             }
             node = new Node(n.A, 0, n.count + 1);
-            if(!map.getOrDefault(node, false)) {
-                map.put(node, true);
+            tmp = map.getOrDefault(node.A, new HashSet<>());
+            if(!tmp.contains(node.B)) {
+                tmp.add(node.B);
                 queue.add(node);
+                map.put(node.A, tmp);
             }
             //M(x,y) : 물통 X to 물통 Y
             node = move(n, true);
-            if(!map.getOrDefault(node, false)) {
-                map.put(node, true);
+            tmp = map.getOrDefault(node.A, new HashSet<>());
+            if(!tmp.contains(node.B)) {
+                tmp.add(node.B);
                 queue.add(node);
+                map.put(node.A, tmp);
             }
             node = move(n, false);
-            if(!map.getOrDefault(node, false)) {
-                map.put(node, true);
+            tmp = map.getOrDefault(node.A, new HashSet<>());
+            if(!tmp.contains(node.B)) {
+                tmp.add(node.B);
                 queue.add(node);
+                map.put(node.A, tmp);
             }
         }
 
@@ -95,13 +132,10 @@ public class Main {
     private static Node move(Node node, boolean check) {
         int x = check?node.A:node.B;
         int y = check?node.B:node.A;
-        //x를 y에 부우려면?
-        //x에 있는 사이즈와 y에 남은 사이즈 중 더 작은 값을 체크하고
-        //x에서 그 값 빼주고
-        //y에서 그 값 더해주고
         int min = Math.min(x, (check?B:A) - y);
-
-        return new Node(check?(x - min):(y + min), check?(y + min):(x - min), node.count + 1);
+        int minusX = x - min;
+        int plusY = y + min;
+        return new Node(check?minusX:plusY, check?plusY:minusX, node.count + 1);
     }
 
     private static int read() throws Exception {
@@ -109,6 +143,5 @@ public class Main {
         while ((d = System.in.read()) > 32)
             o = (o << 3) + (o << 1) + (d & 15);
         return o;
-
     }
 }
