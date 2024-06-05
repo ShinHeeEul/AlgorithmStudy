f59f0be889a1f1841f9a8e920c3b59ad977529d5
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-06-05 18:12:41 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 128 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -1,33 +1,27 @@
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         int N = read();
-
         int count = 0;
         int before = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        Stack<Integer> pq = new Stack<>();
         pq.add(0);
-        // 여기서 pop 했을떄?
         for(int i = 0 ; i < N; i++) {
-            //얘가 pq에 있다면? 냅두고, 없다면 + 1하고 pq에 넣고
             read();
             int y = read();
             if(before < y) count++;
             else {
-                int a = -1;
-                if(!pq.isEmpty()) {
-                    while (( a= pq.peek()) > y) {
-                        pq.poll();
-                        if(pq.isEmpty()) break;
-                    }
+                while (pq.peek() > y) {
+                    pq.pop();
                 }
                 // 있다면?
-                if(a != y) count++;
-                else pq.poll();
+                if(pq.peek() != y) count++;
+                else pq.pop();
             }
-            pq.add(y);
+            pq.push(y);
             before = y;
         }
         System.out.println(count);
