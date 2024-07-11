1de5dde0129e400299fcf70fdfd6889872ed7903
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-11 09:33:25 +0900

    [Gold I] Title: 구간 합 구하기, Time: 340 ms, Memory: 24792 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,102 +1,72 @@
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.FileInputStream;
+
 public class Main {
-    static long[] segment;
-    public static void main(String[] args) throws Exception {
-        long N = read();
-        long M = read();
-        long K = read();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M, K;
+	static long[] tree;
 
-        int size = 1;
-        while(size < N) {
-            size *= 2;
-        }
+	public static void main(String[] args) throws Exception {
+		N = (int) read();
+		M = (int) read();
+		K = (int) read();
+		tree = new long[N+1]; // 1 부터 인덱스로 사용
+        StringBuilder sb = new StringBuilder();
+		
+		for(int i = 1; i <= N; i++) {
+			update(i, read());
+		}
+		
+		for(int i = 0; i < M+K; i++) {
+			int a = (int) read();
+			
+			if(a == 1) { // update
+				int idx = (int) read();
+				long val = read();
+				long diff = val - sum(idx, idx);
+				update(idx, diff);
+			}
+			else if (a == 2) { // sum
+				int left = (int) read();
+				int right = (int) read();
+                sb.append(sum(left, right)).append("\n");
+			}
+		}
+        System.out.println(sb);
+	}
 
-        long[] arr = new long[size + 1];
-        segment = new long[size * 2+1];
-
-
-        for(int i = 1; i <= N; i++) {
-            arr[i] = read();
-            segment[size + i] = arr[i];
-        }
-
-
-        for(int i = size*2-1; i >= 0; i-=2) {
-            segment[i/2 + 1] = segment[i] + segment[i+1];
-        }
-
-        for(int i = 0 ; i < M + K; i++) {
-            long a = read();
-            int b = (int) read();
-            long c = read();
-
-            if(a == 1) {
-                int index = size + b;
-                long val = c - segment[index];
-                while(index >= 2) {
-                    segment[index] += val;
-                    index = ceilDiv(index, 2);
-                }
-                continue;
-            }
-
-            Queue<Node> queue = new LinkedList<>();
-            queue.add(new Node(1, size, 2));
-            long sum = 0;
-            while(!queue.isEmpty()) {
-                Node node = queue.poll();
-                if(node.start >= b && node.end <= c) {
-                    sum += segment[node.index];
-                    continue;
-                }
-                int mid = (node.start + node.end) >> 1;
-                if(b <= mid) {
-                    queue.add(new Node(node.start, mid, node.index * 2 - 1));
-                }
-                if(c > mid) {
-                    queue.add(new Node(mid + 1, node.end, node.index * 2));
-                }
-            }
-            bw.write(sum + "\n");
-        }
-        bw.flush();
-    }
-
-    public static class Node {
-        int start;
-        int end;
-        int index;
-
-        public Node(int start, int end, int index) {
-            this.start = start;
-            this.end = end;
-            this.index = index;
-        }
-    }
-
-    private static int ceilDiv(int x, int y) {
-        return (x + y - 1) / y;
-    }
-
+	private static void update(int i, long diff) { // i번째 값을 tree[i]+diff로 변경
+		while(i < tree.length) {
+			tree[i] += diff;
+			i = i + (i & -i);
+		}
+	}
+	
+	private static long sum(int left, int right) { // left부터 right번째 까지의 구간합
+		return sum(right) - sum(left-1);
+	}
+	
+	private static long sum(int i) { // 1~i번째 까지의 구간합
+		long answer = 0;
+		while (i > 0) {
+			answer += tree[i];
+			i = i - (i & (-i));
+		}
+		return answer;
+	}
+    
     private static long read() throws Exception {
-        long d, o = 0;
-        boolean isNegative = false;
-
+        long d, o;
+        boolean negative = false;
         d = System.in.read();
-        if (d == 45) {
-            isNegative = true;
+
+        if (d == '-') {
+
+            negative = true;
             d = System.in.read();
         }
-
-        d &= 15;
-        o = d;
+        o = d & 15;
         while ((d = System.in.read()) > 32)
             o = (o << 3) + (o << 1) + (d & 15);
 
-        return isNegative ? -o : o;
+        return negative? -o:o;
     }
 }
