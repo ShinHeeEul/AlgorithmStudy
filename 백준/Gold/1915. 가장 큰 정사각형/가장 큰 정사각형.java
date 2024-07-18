ad6b2f655475b5922ceb3b53b2a4d27fa4c8a76f
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-18 18:16:56 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 196 ms, Memory: 18904 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -1,30 +1,24 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
-    
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M  = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+
+        int N = read();
+        int M = read();
         int ans = 1;
-        
+
         int[][] box = new int[N + 1][M + 1];
         boolean b = false;
-        for(int i = 1; i <= N; i++) {
-            String s = br.readLine();
-            for(int j = 1; j <= M; j++) {
-                int a = Integer.parseInt(s.charAt(j-1) + "");
-                if(a == 1) b = true;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                int a = System.in.read() - '0';
+                if (a == 1) b = true;
                 box[i][j] = a;
-                if(box[i-1][j] >= 1 && box[i][j-1] >= 1 && box[i-1][j-1] >= 1 && box[i][j] >= 1) {
-                    box[i][j] = Math.min(box[i][j-1], Math.min(box[i-1][j], box[i-1][j-1])) + 1;
+                if (box[i - 1][j] >= 1 && box[i][j - 1] >= 1 && box[i - 1][j - 1] >= 1 && box[i][j] >= 1) {
+                    box[i][j] = Math.min(box[i][j - 1], Math.min(box[i - 1][j], box[i - 1][j - 1])) + 1;
                 }
                 ans = Math.max(ans, box[i][j]);
             }
+            System.in.read();
         }
         System.out.print(b ? ans * ans : 0);
 
@@ -44,7 +38,7 @@ public class Main {
         while ((d = System.in.read()) > 32)
             o = (o << 3) + (o << 1) + (d & 15);
 
-        return negative? -o:o;
+        return negative ? -o : o;
     }
 
 }
