cf530224a3768ccb759b50a2cda8f23dc84c0e7b
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-18 18:06:14 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 400 ms, Memory: 60904 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -3,31 +3,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[][] box;
-    static int ans = 1;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-       int N = Integer.parseInt(st.nextToken());
-       int M  = Integer.parseInt(st.nextToken());
-
-       box = new int[N + 1][M + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M  = Integer.parseInt(st.nextToken());
+        int ans = 1;
+        
+        int[][] box = new int[N + 1][M + 1];
         boolean b = false;
-       for(int i = 1; i <= N; i++) {
-           String s = br.readLine();
-           for(int j = 1; j <= M; j++) {
-               int a = Integer.parseInt(s.charAt(j-1) + "");
-               if(a == 1) b = true;
-               box[i][j] = a;
-               if(box[i-1][j] >= 1 && box[i][j-1] >= 1 && box[i-1][j-1] >= 1 && box[i][j] >= 1) {
-                   box[i][j] = Math.min(box[i][j-1], Math.min(box[i-1][j], box[i-1][j-1])) + 1;
-               }
-               ans = Math.max(ans, box[i][j]);
-           }
-       }
-        System.out.println(b ? ans * ans : 0);
+        for(int i = 1; i <= N; i++) {
+            String s = br.readLine();
+            for(int j = 1; j <= M; j++) {
+                int a = Integer.parseInt(s.charAt(j-1) + "");
+                if(a == 1) b = true;
+                box[i][j] = a;
+                if(box[i-1][j] >= 1 && box[i][j-1] >= 1 && box[i-1][j-1] >= 1 && box[i][j] >= 1) {
+                    box[i][j] = Math.min(box[i][j-1], Math.min(box[i-1][j], box[i-1][j-1])) + 1;
+                }
+                ans = Math.max(ans, box[i][j]);
+            }
+        }
+        System.out.print(b ? ans * ans : 0);
 
 
     }
