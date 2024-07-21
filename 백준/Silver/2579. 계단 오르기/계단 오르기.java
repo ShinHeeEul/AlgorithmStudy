3226b1ac5c468ceb078ec3ca90470f8ddfd573f5
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-22 08:40:23 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,37 +1,57 @@
-import java.io.*;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
 
-    static ArrayList<Integer> answer;
-    static int[] stair;
-    static boolean[] visit;
-    static int max;
-    static int N;
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-
-        stair = new int[N+1];
-        visit = new boolean[N+1];
-        int[][] S = new int[N+1][N+1];
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int[] arr = new int[N];
+        int[][] dp = new int[2][400];
 
         for(int i = 0; i < N; i++) {
-            stair[i] = Integer.parseInt(br.readLine());
+            arr[i] = read();
         }
-        S[0][0] = stair[0];
-        S[1][0] = stair[1];
-        S[1][1] = stair[0] + stair[1];
+
+        if(N == 1) {
+            System.out.println(arr[0]);
+            return;
+        }
+
+        if(N == 2) {
+            System.out.println(arr[1] + arr[0]);
+            return;
+        }
+
+        dp[0][0] = arr[0];
+        dp[0][1] = arr[1];
+        dp[1][1] = arr[0] + arr[1];
 
         for(int i = 2; i < N; i++) {
-            S[i][0] = Math.max(S[i-2][0] + stair[i], S[i-2][1] + stair[i]);
-            S[i][1] = S[i-1][0] + stair[i];
+            dp[0][i] = Math.max(dp[0][i], dp[0][i-2] + arr[i]);
+            dp[0][i] = Math.max(dp[0][i], dp[1][i-2] + arr[i]);
+            dp[1][i] = Math.max(dp[0][i], dp[0][i-1] + arr[i]);
         }
 
-        System.out.println(Math.max(S[N-1][0], S[N-1][1]));
+        System.out.println(Math.max(dp[0][N-1], dp[1][N-1]));
+
+
+    }
+
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == 45) {
+            negative = true;
+            d = System.in.read();
         }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative ? -o : o;
+    }
+
 }
