7f8ef7ac94d34053c5a5a301c731bf703ee5ee0b
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-22 08:42:54 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,60 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class Main {
 
+    static ArrayList<Integer> answer;
+    static int[] stair;
+    static boolean[] visit;
+    static int max;
+    static int N;
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int[][] dp = new int[2][400];
+
+        N = Integer.parseInt(br.readLine());
+
+        stair = new int[N+1];
+        visit = new boolean[N+1];
+        int[][] S = new int[N+1][N+1];
 
         for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            stair[i] = Integer.parseInt(br.readLine());
         }
-
-        if(N == 1) {
-            System.out.println(arr[0]);
-            return;
-        }
-
-        if(N == 2) {
-            System.out.println(arr[1] + arr[0]);
-            return;
-        }
-
-        dp[0][0] = arr[0];
-        dp[0][1] = arr[1];
-        dp[1][1] = arr[0] + arr[1];
+        S[0][0] = stair[0];
+        S[1][0] = stair[1];
+        S[1][1] = stair[0] + stair[1];
 
         for(int i = 2; i < N; i++) {
-            dp[0][i] = Math.max(dp[0][i], dp[0][i-2] + arr[i]);
-            dp[0][i] = Math.max(dp[0][i], dp[1][i-2] + arr[i]);
-            dp[1][i] = Math.max(dp[0][i], dp[0][i-1] + arr[i]);
+            S[i][0] = Math.max(S[i-2][0] + stair[i], S[i-2][1] + stair[i]);
+            S[i][1] = S[i-1][0] + stair[i];
         }
 
-        System.out.println(Math.max(dp[0][N-1], dp[1][N-1]));
-
-
-    }
-
-
-    private static int read() throws Exception {
-        int d, o;
-        boolean negative = false;
-        d = System.in.read();
-        if (d == 45) {
-            negative = true;
-            d = System.in.read();
+        System.out.println(Math.max(S[N-1][0], S[N-1][1]));
         }
-
-        o = d & 15;
-        while ((d = System.in.read()) > 32)
-            o = (o << 3) + (o << 1) + (d & 15);
-
-        return negative ? -o : o;
-    }
-
 }
