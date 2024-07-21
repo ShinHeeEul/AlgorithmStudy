54cf7000489d98e0b9cf9fce98af1f85b15a920f
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-22 08:41:14 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,15 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        int N = read();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
         int[][] dp = new int[2][400];
 
         for(int i = 0; i < N; i++) {
-            arr[i] = read();
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         if(N == 1) {
