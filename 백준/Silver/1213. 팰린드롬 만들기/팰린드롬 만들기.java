201b12f6c179263267147289363705a5f3e6dedc
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-24 15:37:46 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 164 ms, Memory: 16436 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,53 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
 
+    static HashMap<Character, Integer> map = new HashMap<>();
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        HashMap<Character, Integer> map = new HashMap<>();
 
-        for (char c : s.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
-
-        boolean b = false;
-        char cc = ' ';
-        for (char c : map.keySet()) {
-            int i = map.get(c);
-
-            if (i % 2 != 0 && !b) {
-                cc = c;
-                b = true;
-            } else if (i % 2 != 0) {
-                System.out.println("I'm Sorry Hansoo");
-                return;
-            }
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
+        boolean b = true;
+        Character[] cs = new Character[map.size()];
 
-        Character[] array = map.keySet().stream()
-                .sorted(Comparator.reverseOrder())
-                .toArray(Character[]::new);
-
-
-        String ss = "";
-        if(b) {
-            ss += cc;
-            map.put(cc, map.get(cc)- 1);
+        int i = 0;
+        int count = 0;
+        for(char c : map.keySet()) {
+            if(map.get(c) % 2 != 0) count++;
+            cs[i++] = c;
         }
 
-            for(char c : array) {
-                while(map.get(c) != 0) {
-                    ss = c + ss;
-                    ss = ss + c;
-                    map.put(c, map.get(c) - 2);
-                }
-            }
+        if(count > 1) {
+            System.out.println("I'm Sorry Hansoo");
+            return;
+        }
 
-        System.out.println(ss);
+        Arrays.sort(cs);
+
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
+        for(char c : cs) {
+            int a = map.get(c);
+            if(a % 2 != 0) ans.append(c);
+            a >>= 1;
+            while(a --> 0) sb.append(c);
+        }
+
+        System.out.println(sb.toString() + ans + sb.reverse());
+    }
+
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == 45) {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative ? -o : o;
     }
 
 }
