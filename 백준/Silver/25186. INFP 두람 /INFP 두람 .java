d4bacdfc0a6fccc52836cc3330004f73bf16a2d1
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-25 17:37:12 +0900

    [Silver II] Title: INFP 두람 , Time: 152 ms, Memory: 14760 KB -BaekjoonHub

--- 백준/Silver/25186. INFP 두람 /INFP 두람 .java
+++ 백준/Silver/25186. INFP 두람 /INFP 두람 .java
@@ -3,23 +3,24 @@ class Main {
     public static void main(String[] args) throws Exception {
         int N = read();
 
-        long sum = 0;
+        double sum = 0;
         int[] arr = new int[N];
         for(int i = 0; i < N; i++) {
             int a = read();
             sum += a;
             arr[i] = a;
         }
-        
-        float k = (float) (sum + 1) / 2;
-        
+
         if(sum == 1) {
             System.out.println("Happy");
             return;
         }
+        
+        sum = (sum + 1) / 2;
+
 
         for(int i = 0; i < N; i++) {
-            if(k <= arr[i]) {
+            if(sum <= arr[i]) {
                 System.out.println("Unhappy");
                 return;
             }
