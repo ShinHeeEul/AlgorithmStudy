eab1e35c307f84b2c6c64d1e221e7c9165a0a640
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-07-26 13:31:42 +0900

    [Gold II] Title: 저울, Time: 132 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,44 @@
+import java.util.PriorityQueue;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        int N = read();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < N; i++) {
+            pq.add(read());
+        }
+
+        long sum = 1;
+        while(!pq.isEmpty()) {
+            int a = pq.poll();
+
+            if(sum < a) {
+                System.out.println(sum);
+                return;
+            }
+            sum += a;
+        }
+        System.out.println(sum);
+    }
+
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == 45) {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative ? -o : o;
+    }
+}
