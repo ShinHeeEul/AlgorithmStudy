dcf0e202a7e4f77512b3e74f1000c5aec06454e2
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-08-12 08:24:27 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 7.63 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -19,52 +19,25 @@ class Solution {
         long beforeMinDegree = startMinDegree;
         long beforeSecondDegree = startSecondDegree;
         
-        int let = 0;
         while(count != 0) {
-            boolean b = true;
             count--;
-            
-            if((startHourDegree == 0 || startHourDegree == 35985600) && startMinDegree == 0 && startSecondDegree == 0) {
-                startHourDegree = 0;
-                b = false;
-            }
-            
-            if((startHourDegree == 0 || startHourDegree == 35984767) && startMinDegree == 35990000 && startSecondDegree == 35400000) {
-                System.out.println("dfs");
-            }
-            
-            if(let== 0) {
-                System.out.println("21second : " + startSecondDegree + " min : " + startMinDegree +  " erere : " + startHourDegree);
-            }
-
-                
-            
-            let++;
-            
             startHourDegree = (startHourDegree + hourDegree)  % 36000000;
             startMinDegree = (startMinDegree + minDegree) % 36000000;
             startSecondDegree = (startSecondDegree + secondDegree) % 36000000;
             
-            
-            if(b) {
-            
             if((startSecondDegree == 0 && startMinDegree > 35400000) ||
               (startSecondDegree == 0 && startHourDegree > 35400000)){
-                System.out.println("aaab");
                 answer++;
             } else { 
                 if(beforeMinDegree > beforeSecondDegree &&
               startMinDegree <= startSecondDegree) {
-                System.out.println("aaac");
                 answer++;
             }
             if(beforeHourDegree > beforeSecondDegree &&
                startHourDegree <= startSecondDegree) {
-                System.out.println("aaad");
                 answer++;
             }
                    }
-            }
             
             if(startHourDegree == 35985600) startHourDegree = 0;
             beforeHourDegree = startHourDegree;
