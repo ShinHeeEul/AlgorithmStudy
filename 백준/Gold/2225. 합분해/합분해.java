decb53840c742d9e96683065ff8b750a13fb52e0
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2024-10-15 20:17:41 +0900

    [Gold V] Title: 합분해, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -4,18 +4,28 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int N = read();
         int K = read();
-        int[][] dp = new int[N + 1][K + 1];
-        Arrays.fill(dp[0], 1);
-        dp[0][0] = 0;
-        for(int i = 0; i <= N ; i++) dp[i][1] = 1;
+        if(K == 0) {
+            System.out.println(0);
+            return;
+        } else if(K == 1) {
+            System.out.println(1);
+            return;
+        } else if(K == 2) {
+            System.out.println(N + 1);
+            return;
+        }
 
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= K; j++) {
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % (1_000_000_000);
+        int[][] dp = new int[N][K - 1];
+
+        for(int i = 0; i < N ; i++) dp[i][0] = (i + 2);
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 1; j < K-1; j++) {
+                dp[i][j] = (((i-1) < 0 ? 1 : dp[i-1][j]) + dp[i][j-1]) % (1_000_000_000);
             }
         }
 
-        System.out.println(dp[N][K]);
+        System.out.println(dp[N - 1][K - 2]);
     }
 
     private static int read() throws Exception {
