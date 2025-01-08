47b9b2f25dfced5cf5cb2678fb5761d007ebba83
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2025-01-08 19:10:16 +0900

    [D5] Title: 숫자 게임, Time: 124 ms, Memory: 20,224 KB -BaekjoonHub

---
+++ SWEA/D5/7206. 숫자 게임/README.md
@@ -0,0 +1,15 @@
+# [D5] 숫자 게임 - 7206 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWlGyBQqaEgDFASG) 
+
+### 성능 요약
+
+메모리: 20,224 KB, 시간: 124 ms, 코드길이: 1,261 Bytes
+
+### 제출 일자
+
+2025-01-08 19:10
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/7206. 숫자 게임/숫자 게임.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+    static int[] dp;
+    static int a;
+    static Stack<Node> stack;
+
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        dp = new int[100000];
+        for(int i = 10; i <= 99999; i++) {
+            backTracking(i, i, 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int t = 1; t <= T; t++) {
+
+            a = Integer.parseInt(br.readLine());
+            sb.append("#").append(t).append(" ").append(dp[a]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void backTracking(int current, int val, int multi) {
+
+        dp[current] = Math.max(dp[current], dp[val * multi] + 1);
+
+        if(val < 10) return;
+        int radix = 10;
+        // 99999 -> 9999 9 -> 9 9 9 9 9
+        while(radix < val) {
+            backTracking(current, val / radix, multi * (val % radix));
+            radix *= 10;
+        }
+    }
+
+
+
+    public static class Node {
+        int val;
+        int count;
+        Node(int val, int count) {
+            this.val = val;
+            this.count = count;
+        }
+    }
+}
