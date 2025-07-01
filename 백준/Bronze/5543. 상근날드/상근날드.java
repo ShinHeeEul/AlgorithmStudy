e37b4d3d9530ff7760f8bcd2da0fe1ca62e98841
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2025-07-01 20:02:26 +0900

    [Bronze IV] Title: 상근날드, Time: 100 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(Math.min(read(), Math.min(read(), read())) + Math.min(read(), read()) - 50);
+    }
+
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+
+        if (d == '-') {
+
+            negative = true;
+            d = System.in.read();
+        }
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative? -o:o;
+    }
+}
