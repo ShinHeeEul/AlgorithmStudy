757195fb0ae5c4169b49713c0cc10e6f2be4775d
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2025-07-04 23:59:04 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5412 ms, Memory: 405384 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,32 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger b = new BigInteger(st.nextToken());
+        BigInteger c = new BigInteger(st.nextToken());
+
+        System.out.println(b.multiply(c));
+    }
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == 45) {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative ? -o : o;
+    }
+
+}
