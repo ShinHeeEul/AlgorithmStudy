547c497a0d7bef0f9bd68d57457cb50a27bdf413
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2025-07-04 23:23:18 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(506);
+        System.out.println("tlsgmldmf");
+    }
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == 45) {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative ? -o : o;
+    }
+
+}
