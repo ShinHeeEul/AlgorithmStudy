450a40815d91fb3fd86cb0e89260a92991708f3f
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2025-07-04 23:25:41 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 15812 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        while(N-->0) {
+            String s = br.readLine();
+            System.out.println("" + s.charAt(0) + s.charAt(s.length()-1));
+        }
+    }
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == 45) {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative ? -o : o;
+    }
+
+}
