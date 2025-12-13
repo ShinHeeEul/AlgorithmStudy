69b145712b8c496ecbd5f9a9a45059fbc1c6dc3d
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2025-12-13 14:23:00 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 272 ms, Memory: 20740 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,44 +1,39 @@
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-import java.util.concurrent.atomic.AtomicInteger;
-
-
 public class Main {
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws IOException {
+        int N = read();
+        int M = read();
 
+        int[] sum = new int[N + 1];
 
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(input);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        ArrayList<Integer> list = new ArrayList<>();
-
-        st = new StringTokenizer(br.readLine(), " ");
-        list.add(0);
-        for(int i = 0; i < N; i++) {
-            list.add(list.get(i) + Integer.parseInt(st.nextToken()));
+        for(int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + read();
         }
-        for(int i = 0; i < M; i++) {
-            AtomicInteger sum = new AtomicInteger();
-            st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
 
-            bw.write(list.get(end) - list.get(start - 1) + "\n");
+        StringBuilder sb = new StringBuilder();
+
+        while(M --> 0) {
+            int a = read();
+            int b = read();
+            sb.append(sum[b] - sum[a - 1]).append("\n");
         }
-        bw.flush();
 
-        input.close();
-        br.close();
-        bw.close();
+        System.out.println(sb);
+    }
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == '-') {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative? -o:o;
     }
 }
