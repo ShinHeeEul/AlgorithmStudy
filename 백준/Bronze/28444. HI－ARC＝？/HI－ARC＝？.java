c4d3fda76bf2ac4fc535c2a5aee236a61eb5e2fe
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2026-01-19 00:07:07 +0900

    [Bronze V] Title: HI-ARC=?, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(read() * read() - (read() * read() * read()));
+    }
+
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == '-') {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative? -o:o;
+    }
+}
