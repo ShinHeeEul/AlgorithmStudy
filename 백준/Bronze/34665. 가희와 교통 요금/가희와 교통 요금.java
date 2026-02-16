e5c7ed6a577e339b6b8deea29c5bdda91c11050b
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2026-02-16 18:32:22 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 88 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        if(s1.equals(s2)) {
+            System.out.println(0);
+        } else {
+            System.out.println(1550);
+        }
+    }
+
+    private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == '-') {
+            negative = true;
+            d = System.in.read();
+        }
+
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+
+        return negative? -o:o;
+    }
+}
